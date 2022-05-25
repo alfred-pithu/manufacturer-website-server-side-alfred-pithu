0544d81d28e03500549278153d1adcbd67d168b8
unknown <alfredpithu@gmail.com>
2022-05-25 08:38:07 +0600

    create jwt done

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+var jwt = require('jsonwebtoken');
+
 
 
 //middleware
@@ -13,6 +15,7 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7bgws.mongodb.net/?retryWrites=true&w=majority`;
 
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -21,10 +24,38 @@ async function run() {
 
         //collections
         const productCollection = client.db('assignment-12').collection('products');
+        const feedbackCollection = client.db('assignment-12').collection('feedbacks');
+
+        //to get all the products
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find({}).toArray();
+            res.send(result)
+        })
 
 
 
 
+
+
+        //to get all the feedbacks
+        app.get('/feedbacks', async (req, res) => {
+            const result = await feedbackCollection.find({}).toArray();
+            res.send(result);
+        })
+
+        //to add new feedback
+        app.post('/feedbacks', async (req, res) => {
+
+        })
+
+
+        //create JWT 
+        app.put('/token', async (req, res) => {
+            const email = req.query;
+            const payload = { email: email }
+            const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
 
 
 
@@ -45,7 +76,7 @@ async function run() {
 
     }
     finally {
-        await client.close();
+        // await client.close();
     }
 }
 
