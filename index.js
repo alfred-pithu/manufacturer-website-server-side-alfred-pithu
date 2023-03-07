bf98e04c851bd617bab26d5af1886a43e2babba2
Alfred Pithu <alfredpithu@gmail.com>
2023-03-07 11:17:26 +0600

    minor changes

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7bgws.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://assignment-12:H81v5WXyJbQ6FSKD@cluster0.7bgws.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7bgws.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 //verify jwt function 
